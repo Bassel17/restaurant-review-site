3f2daaea13e77341b69afc8b45890d553e6098b4
Bassel <basselkanso82@gmail.com>
2020-06-02 00:08:08 +0300

    Feature: get location of user

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export const App = (props) => {
+
+    const [positionState, setPositionState] = useState({
+      lat: 40.854885,
+      lng: -88.081807
+    });
+
+    const [places, setPlaces] = useState("");
+
+    useEffect(() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setPositionState({
+          lat:position.coords.latitude,
+          lng:position.coords.longitude
+        });
+      });
+      fetchData()
+    });
+
+    const fetchData = async () => {
+      try{
+        const response = await fetch (`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAyHplLn72iIDSgWZrwaWz3XDvgJmGhNsc&location=${positionState.lat},${positionState.lng}&radius=50000&type=restaurant`,{
+        method: 'GET',  
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        Headers:{
+          'Access-Control-Allow-Origin':'http://localhost:3000'
+        }
+        });
+        const results = await response.json();
+        console.log(results);
+        setPlaces(results);
+      }catch(error){
+        console.log(error);
+      }
+    };
+    return (
+      <Map 
+        google={props.google} 
+        zoom={15}
+        initialCenter={positionState}
+        center={positionState}
+      >
+ 
+        <Marker 
+                position={positionState} name={'Current location'} />
+ 
+        <InfoWindow>
+            <div>
+              <h1>ok</h1>
+            </div>
+        </InfoWindow>
+      </Map>
   );
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyAyHplLn72iIDSgWZrwaWz3XDvgJmGhNsc")
+})(App)
